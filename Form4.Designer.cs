6c471bc9427875f969545e2852408c7a06cf4a3d
stasyandr <45917470+stasyandr@users.noreply.github.com>
2021-09-24 01:35:16 +0300

    Add task3

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -28,8 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать картинку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 167);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(54, 350);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(221, 350);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 56);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(397, 350);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 56);
+            this.trackBar3.TabIndex = 4;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "H";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "S";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(429, 389);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "V";
             // 
             // button4
             // 
@@ -47,16 +129,36 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.button4);
             this.Name = "Form4";
             this.Text = "Form4";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form4_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button4;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -28,5 +28,167 @@ namespace CompGraf2
             this.Visible = false;
             main.Visible = true;
         }
+        double my_H = 0;
+        double my_S = 0;
+        double my_V = 0;
+        Bitmap pict;
+
+
+        List<double> HSV_Transformer_From_RGB(double R, double G, double B)
+        {
+
+
+            List<double> l = new List<double>();
+
+            double maximum = Math.Max(Math.Max(R, G), B);
+            double minimum = Math.Min(Math.Min(R, G), B);
+            double difference = maximum - minimum;
+
+            double H;
+            if (Math.Abs(maximum - minimum) < 0.0001)
+                H = 0;
+            else
+            if (Math.Abs(maximum - R) < 0.0001 && G >= B)
+                H = 60 * (G - B) / difference;
+            else
+            if (Math.Abs(maximum - B) < 0.0001 && G < B)
+                H = 60 * (G - B) / difference + 360;
+            else
+            if (Math.Abs(maximum - G) < 0.0001)
+                H = 60 * (B - R) / difference + 120;
+            else
+                H = 60 * (R - G) / difference + 240;
+            double S;
+
+            if (Math.Abs(maximum - 0) < 0.0001)
+                S = 0;
+            else
+                S = 1 - minimum / maximum;
+
+            double V = maximum;
+
+            l.Add(H);
+            l.Add(S);
+            l.Add(V);
+
+            return l;
+        }
+
+
+
+        List<double> RGB_Transformer_From_HSV(double H, double S, double V)
+        {
+
+            double helper = Math.Floor(H / 60.0) % 6;
+            double x = H / 60.0 - Math.Floor(H / 60.0);
+            double y = (1 - S) * V;
+            double z = (1 - x * S) * V;
+            double q = (1 - (1 - x) * S) * V;
+            List<double> l = new List<double>();
+            switch (helper)
+            {
+                case 0:
+                    l.Add(V);
+                    l.Add(q);
+                    l.Add(y);
+                    return l;
+                case 1:
+                    l.Add(z);
+                    l.Add(V);
+                    l.Add(y);
+                    return l;
+                case 2:
+                    l.Add(y);
+                    l.Add(V);
+                    l.Add(q);
+                    return l;
+                case 3:
+                    l.Add(y);
+                    l.Add(z);
+                    l.Add(V);
+                    return l;
+                case 4:
+                    l.Add(q);
+                    l.Add(y);
+                    l.Add(V);
+                    return l;
+                case 5:
+                    l.Add(V);
+                    l.Add(y);
+                    l.Add(z);
+                    return l;
+                default:
+
+                    return l;
+            }
+        }
+
+
+        void Draw_and_Save()
+        {
+            double loc_H = trackBar1.Value;
+            double loc_S = trackBar2.Value / 100.0;
+            double loc_V = trackBar3.Value / 100.0;
+
+            Bitmap myBitmap = (Bitmap)pict.Clone();
+
+            for (int i = 0; i < myBitmap.Width; ++i)
+                for (int j = 0; j < myBitmap.Height; ++j)
+                {
+                    var pixel_color = myBitmap.GetPixel(i, j);
+                    var res = HSV_Transformer_From_RGB(pixel_color.R / 255.0, pixel_color.G / 255.0, pixel_color.B / 255.0);
+
+
+                    res[0] += loc_H;
+
+                    if (res[0] > 360)
+                        res[0] -= 360;
+                    if (res[0] < 0)
+                        res[0] += 360;
+                    res[1] += loc_S;
+                    res[1] = Math.Min(1, res[1]);
+                    res[1] = Math.Max(0, res[1]);
+                    res[2] += loc_V;
+                    res[2] = Math.Min(1, res[2]);
+                    res[2] = Math.Max(0, res[2]);
+
+                    var res_back = RGB_Transformer_From_HSV(res[0], res[1], res[2]);
+                    myBitmap.SetPixel(i, j, Color.FromArgb(pixel_color.A, (int)(res_back[0] * 255), (int)(res_back[1] * 255), (int)(res_back[2] * 255)));
+                }
+
+            pictureBox1.Image = myBitmap;
+            myBitmap.Save("my_final.jpg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openner = new OpenFileDialog
+            {
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"
+            };
+            if (openner.ShowDialog() == DialogResult.OK)
+            {
+                pict = new Bitmap(openner.FileName);
+                pictureBox1.Image = pict;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            my_H = trackBar1.Value * 1.5;
+            Draw_and_Save();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            my_S = trackBar2.Value / 100.0;
+            Draw_and_Save();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            my_V = trackBar3.Value / 100.0;
+            Draw_and_Save();
+        }
     }
 }
